0cc852782635f679823bd9ecd20b433737b4e003
Thibaud SOWA <thibaudsowa@gmail.com>
2019-02-28 19:22:13 +0100

    writer

--- filewriter.js
+++ filewriter.js
@@ -1,9 +1,29 @@
 let fs = require('fs-extra');
 
 
+let mockProcess = function (type) {
+    return [[1], [4, 5], [2], [6, 3]];
+};
+
 let writeInDir = function (data, dir, filename) {
 
-    fs.appendFile(dir + '/' + filename, data).then((err) => console.log(err));
+    let dataString = '';
+
+    dataString += data.length + '\n';
+
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+            dataString += data[i][j] + ' ';
+        }
+        dataString = dataString.trim();
+
+        if(i !== data.length -1) {
+            dataString += '\n';
+        }
+    }
+
+
+    fs.appendFile(dir + '/' + filename, dataString).then((err) => console.log(err));
 
 };
 
@@ -15,9 +35,11 @@ exports.write = function () {
     fs.ensureDir(dir).then((err) => {
         console.log(err);
 
-        writeInDir('test', dir, 'file1');
-        writeInDir('test', dir, 'file2');
-        writeInDir('test', dir, 'file3');
+        writeInDir(mockProcess('A'), dir, 'A');
+        writeInDir(mockProcess('B'), dir, 'B');
+        writeInDir(mockProcess('C'), dir, 'C');
+        writeInDir(mockProcess('D'), dir, 'D');
+        writeInDir(mockProcess('E'), dir, 'E');
     });
 
 

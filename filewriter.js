4992824fa3b07eb8a52dc35388f6d9cb8b271b85
Léo GERBERON <leo.gerberon@lacooperativewelcoop.com>
2019-02-28 20:27:00 +0100

    dumb

--- filewriter.js
+++ filewriter.js
@@ -1,6 +1,8 @@
 let fs = require('fs-extra');
 let logique = require('./LogiqueDumb.js')
 var parser = require('./parser.js');
+let archiver = require('archiver');
+
 let mockProcess = function (type) {
     return [[1], [4, 5], [2], [6, 3]];
 };
@@ -17,29 +19,39 @@ let writeInDir = function (data, dir, filename) {
         }
         dataString = dataString.trim();
 
-        if(i !== data.length -1) {
+        if (i !== data.length - 1) {
             dataString += '\n';
         }
     }
 
 
-    fs.appendFile(dir + '/' + filename, dataString).then((err) => console.log(err));
+    return fs.appendFile(dir + '/' + filename, dataString).then((err) => console.log(err));
 
 };
 
 exports.write = function () {
 
     let now = new Date().getTime();
-    let dir = './output/' + now;
+    let dir = 'output/' + now;
 
-    fs.ensureDir(dir).then((err) => {
-        console.log(err);
+    fs.ensureDir(dir).then(() => {
 
         writeInDir(logique.logique(parser.parse('input/A')), dir, 'A');
         writeInDir(logique.logique(parser.parse('input/B')), dir, 'B');
         /**writeInDir(logique.logique(parser.parse('input/C')), dir, 'C');
         writeInDir(logique.logique(parser.parse('input/D')), dir, 'D');
         writeInDir(logique.logique(parser.parse('input/E')), dir, 'E');**/
+
+        // Do in promise
+
+        var archive = archiver('zip', {
+            zlib: {level: 9} // Sets the compression level.
+        });
+
+        var output = fs.createWriteStream(__dirname + '/' + dir + '/app.zip');
+        archive.pipe(output);
+        archive.directory('app', false);
+        archive.finalize();
     });
 
 

b1818beeaac902e5bcd65f79173855bd1c206585
Léo GERBERON <leo.gerberon@lacooperativewelcoop.com>
2019-02-28 20:37:13 +0100

    coucou

--- LogiqueDumb.js
+++ LogiqueDumb.js
@@ -1,24 +1,23 @@
 exports.logique = function (photos) {
 
     function filterPhoto(photo, lettre) {
-        return photo.verticality;
+        return photo.verticality === lettre;
     }
 
-    var horizontals = photos.photos.filter(data => filterPhoto(data, "V"))
-    var verticals = photos.photos.filter(data => filterPhoto(data, "H"))
+    var horizontals = photos.photos.filter(data => filterPhoto(data, "H"))
+    var verticals = photos.photos.filter(data => filterPhoto(data, "V"))
 
     var slides = horizontals.length + (verticals.length / 2)
     var mySlideshowHorizontal = horizontals.map(function (photo) {
         return [photo.id];
     });
-    
-    
+
+
     var mySlideshowVertical = [];
-    for (let i = 0; i < verticals.length - 1; i=i + 2) {
+    for (let i = 0; i < verticals.length - 1; i = i + 2) {
         mySlideshowVertical.push([verticals[i].id, verticals[i + 1].id])
     }
-    
-    
-    
+
+
     return mySlideshowVertical.concat(mySlideshowHorizontal);
 };
